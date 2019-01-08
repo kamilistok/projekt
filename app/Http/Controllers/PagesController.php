e897e870e28cc9959fa5d16172f398ff735fcbda
Kamil <kamil.m.istok@gmail.com>
2019-01-08 14:35:13 +0100

    08-01-2019

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PagesRequest;
+use App\Pages;
+use App\User;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pages = Pages::orderBy('id', 'DESC')->paginate(2);
+
+        return view('pages.index', compact('pages'));
+
+    }
+
+    /**
+     * Show the form for creatin    g a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pages.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  PagesRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PagesRequest $request)
+    {
+        Pages::create($request->all());
+        return redirect()->route('pages.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pages $page
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pages $page)
+    {
+        return view('pages.edit', compact('page'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  PagesRequest  $request
+     * @param  Pages $page
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PagesRequest $request, Pages $page)
+    {
+        $page->update($request->all());
+        return redirect()->route('pages.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Pages $page
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pages $page)
+    {
+        $page->delete();
+        return redirect()->route('pages.index');
+    }
+}
